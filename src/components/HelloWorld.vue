48a70f3f66f7314c943f94f0bd63b9782742b9a6
Rayen Benzid <rayenbenzid1@gmail.com>
2025-03-06 15:17:36 +0100

    NavBar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,131 +1,39 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">What's next?</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app>
+    <v-app-bar color="white">
+      <v-btn text to="/agenda">أجندة</v-btn>
+      <v-btn text to="/islam">دين الإسلام</v-btn>
+      <v-btn text to="/culture">الثقافة التونسية</v-btn>
+      <v-btn text to="/time-management">إدارة الوقت</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn text to="">الصفحة الرئيسية</v-btn>
+      <img :src="logo" alt="logo" height="60px" width="60px" />
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
+import logo from '../assets/logo.png'; // Importez l'image ici
 
-  data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader/tree/next",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify/tree/next",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
-    importantLinks: [
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Roadmap",
-        href: "https://vuetifyjs.com/introduction/roadmap/",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
-  }),
+export default {
+  name: "App",
+  data() {
+    return {
+      logo: logo, // Utilisez l'image importée
+    };
+  },
 };
 </script>
+
+<style>
+.v-btn {
+  font-size: 22px;
+  font-weight: bold;
+  font-family: "Segoe UI", sans-serif;
+  color: #152538;
+  transition: color 0.3s ease-in-out;
+}
+</style>
