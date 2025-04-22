2aea7d6f078ae0816c9cd76ff4e22da4a5d83669
Rayen Benzid <rayenbenzid1@gmail.com>
2025-04-22 14:26:13 +0100

    Event backend

--- Backend/src/main/java/app/chbebiwin/backend/controllers/WorkshopEventsController.java
+++ Backend/src/main/java/app/chbebiwin/backend/controllers/WorkshopEventsController.java
@@ -3,8 +3,14 @@ package app.chbebiwin.backend.controllers;
 import app.chbebiwin.backend.entities.WorkshopEvents;
 import app.chbebiwin.backend.services.WorkshopEventsService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/WorkshopEvents")
@@ -27,10 +33,12 @@ public class WorkshopEventsController {
     public WorkshopEvents get(@PathVariable Long id) {
         return workshopEventsService.getWorkshopEventsById(id);
     }
+
     @GetMapping("/getAll")
     public List<WorkshopEvents> getAll() {
         return workshopEventsService.getAllWorkshopEvents();
     }
+
     @PutMapping("/update/{id}")
     public WorkshopEvents update(@PathVariable Long id, @RequestBody WorkshopEvents workshopEvents) {
         return workshopEventsService.updateWorkshopEvents(id,workshopEvents);
@@ -38,5 +46,18 @@ public class WorkshopEventsController {
     @DeleteMapping("/deleteAll")
     public String deleteAll(){
         return workshopEventsService.deleteAllWorkshopEvents();
+    }
+
+    @PostMapping("/upload-image")
+    public String uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        Path path = Paths.get("uploads/" + filename);
+        Files.write(path, file.getBytes());
+        return filename; // ou un lien complet genre "http://localhost:9090/uploads/" + filename
+    }
+
+    @DeleteMapping("/deleteExpired")
+    public String deleteExpired() {
+        return workshopEventsService.deletePastWorkshopEvents();
     }
 }
