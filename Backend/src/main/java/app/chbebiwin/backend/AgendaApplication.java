d067b6121b4debf5bfe3e25203be357d620346ef
bejaouimeriem <bejaoui.meriyem@gmail.com>
2025-04-23 00:51:37 +0100

    agenda_back

--- Backend/src/main/java/app/chbebiwin/backend/AgendaApplication.java
+++ Backend/src/main/java/app/chbebiwin/backend/AgendaApplication.java
@@ -1,4 +1,11 @@
 package app.chbebiwin.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AgendaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaApplication.class, args);
+    }
 }
